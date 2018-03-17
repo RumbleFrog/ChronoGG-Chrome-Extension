4decf37d8503bb1acf3074eb09750f8ffacff5ac
RumbleFrog <xxlosttechxx@gmail.com>
2018-03-16 22:54:53 -0400

    :construction: Flow

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,33 @@
-const webpack = require("webpack");
-const path = require('path');
+const webpack = require("webpack")
+    , path = require('path')
+    , CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: {
-        popup: path.join(__dirname, 'src/index.ts'),
+        index: path.join(__dirname, 'src', 'index.ts'),
     },
     output: {
-        path: path.join(__dirname, 'dist/js'),
+        path: path.join(__dirname, 'dist'),
         filename: '[name].js'
     },
     module: {
-        loaders: [{
-            exclude: /node_modules/,
-            test: /\.tsx?$/,
-            loader: 'ts-loader'
-        }]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            }
+        ]
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
     plugins: [
-
-        // pack common vender files
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: Infinity
-        }),
-
-        // exclude locale files in moment
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-
-        // minify
-        // new webpack.optimize.UglifyJsPlugin()
+        new CopyWebpackPlugin([
+            {
+                from: 'src/manifest.json',
+                to: 'manifest.json'
+            }
+        ])
     ]
 };
